ff3247f60eb1f9e66ceeb499d562ca58f37ebf37
PetraBelcar <pbelcar@foi.hr>
2022-06-16 14:58:31 +0200

    Added JavaScript files to the git ignore

--- JavaScripts/test.ts
+++ JavaScripts/test.ts
@@ -1,21 +1,8 @@
-var firstNumberInput, secondNumberInput, sumNumberInput, addButton;
-
-function addTwoNumbers(firstNumber:number, secondNumber:number):number
+function add(n1 : number, n2 : number)
 {
-    return firstNumber + secondNumber;
+    return n1 + n2;
 }
 
-function putValueIntoSumInput():void
-{
-    sumNumberInput.value = addTwoNumbers(parseInt(firstNumberInput.value), parseInt(secondNumberInput.value)).toString();
-}
-
-document.addEventListener("DOMContentLoaded", function()
-{
-    firstNumberInput = document.getElementById("firstNumber") as HTMLInputElement;
-    secondNumberInput = document.getElementById("secondNumber") as HTMLInputElement;
-    sumNumberInput = document.getElementById("sumNumber") as HTMLInputElement;
-    addButton = document.getElementById("addButton") as HTMLButtonElement;
-
-    addButton.addEventListener("click", putValueIntoSumInput);
-});
+const number1 = 5;
+const number2 = 10.4;
+console.log(add(number1, number2));
