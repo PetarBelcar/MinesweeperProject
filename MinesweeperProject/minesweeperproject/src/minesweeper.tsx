205e2a797daa9be875601c93ca9a10948f681f30
PetraBelcar <pbelcar@foi.hr>
2022-06-21 15:31:26 +0200

    Feat: tiles now changed states when clicked

--- MinesweeperProject/minesweeperproject/src/minesweeper.tsx
+++ MinesweeperProject/minesweeperproject/src/minesweeper.tsx
@@ -6,15 +6,15 @@ function Minsweeper(props : {seed:number}):JSX.Element
     {
         var minefield : JSX.Element[][] = [];
         var numberOfMines = 0;
+        //seedrandom(seed.toString()); Somehow make this seeded. I'm honestly astounded that JavaScript does not have this by default
 
         for (let yAxis = 0; yAxis < 15; yAxis++) {
-            //seedrandom(seed.toString()); Somehow make this seeded. I'm honestly astounded that JavaScript does not have this by default
             minefield[yAxis] = [];
             for (let xAxis = 0; xAxis < 15; xAxis++) {
-                var isMine = (numberOfMines >= 40) ? false : (Math.random() >= 0.2) ? false : true;
+                var isMine = (numberOfMines >= 40) ? false : (Math.random() >= 0.16) ? false : true;
                 numberOfMines += (isMine) ? 1 : 0;
 
-                minefield[yAxis][xAxis] = <MinesweeperTile isMine={isMine}/>
+                minefield[yAxis][xAxis] = <MinesweeperTile isMine={isMine} isClicked={false}/>
             }
             minefield[yAxis][20] = <br></br>;
         }
--- MinesweeperProject/minesweeperproject/src/minesweeperTile.tsx
+++ MinesweeperProject/minesweeperproject/src/minesweeperTile.tsx
@@ -1,7 +1,13 @@
-function MinesweeperTile(props : {numberOfAdjecentMines? : number, isMine : boolean})
+import { useState } from "react";
+
+function MinesweeperTile(props : {numberOfAdjecentMines? : number, isMine : boolean, isClicked : boolean})
 {
+    var [tileIsClicked, setTileIsClicked] = useState(false);
+
+    var setTileToClicked = () => setTileIsClicked(true);
+
     return(
-        <button className="minesweeprTileNotClicked">{(props.isMine) ? "X" : ""}</button>
+        <div className={(tileIsClicked) ? "minesweeprTileClicked" : "minesweeprTileNotClicked"} onClick={setTileToClicked}>{(props.isMine) ? "X" : "O"}</div>
     )
 }
 
