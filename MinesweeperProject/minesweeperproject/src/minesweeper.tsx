d1e86f3bfc7f77faeb4e6ca9acb62a0d2cd6ca7d
PetraBelcar <pbelcar@foi.hr>
2022-06-21 14:57:40 +0200

    Feat: added generation of mine and none mine tiles

--- MinesweeperProject/minesweeperproject/src/minesweeper.tsx
+++ MinesweeperProject/minesweeperproject/src/minesweeper.tsx
@@ -5,15 +5,21 @@ function Minsweeper(props : {seed:number}):JSX.Element
     function GenerateMinefield(seed : number) : JSX.Element[][]
     {
         var minefield : JSX.Element[][] = [];
+        var numberOfMines = 0;
 
         for (let yAxis = 0; yAxis < 15; yAxis++) {
+            //seedrandom(seed.toString()); Somehow make this seeded. I'm honestly astounded that JavaScript does not have this by default
             minefield[yAxis] = [];
             for (let xAxis = 0; xAxis < 15; xAxis++) {
-                minefield[yAxis][xAxis] = <MinesweeperTile isMine={(seed % 45) ? false : true}/>
+                var isMine = (numberOfMines >= 40) ? false : (Math.random() >= 0.2) ? false : true;
+                numberOfMines += (isMine) ? 1 : 0;
+
+                minefield[yAxis][xAxis] = <MinesweeperTile isMine={isMine}/>
             }
             minefield[yAxis][20] = <br></br>;
         }
-
+        
+        console.log(numberOfMines);
         return minefield;
     }
 
